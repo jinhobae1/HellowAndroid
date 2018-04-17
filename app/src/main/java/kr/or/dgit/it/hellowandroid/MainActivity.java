131a85d607dac05e3f833a6963db0732c555a342
jinhobae1 <pookgoon@naver.com>
2018-04-17 10:17:19 +0900

    Hello Android 초기화잡업 완료

--- app/src/main/java/kr/or/dgit/it/hellowandroid/MainActivity.java
+++ app/src/main/java/kr/or/dgit/it/hellowandroid/MainActivity.java
@@ -2,6 +2,8 @@ package kr.or.dgit.it.hellowandroid;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Toast;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -9,5 +11,11 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+    }
+
+    public void mBtnClick(View view) {
+
+        Toast.makeText(this,"버튼을 클릭하였습니다.하드코딩",Toast.LENGTH_LONG).show();
+        Toast.makeText(this,R.string.btnClickMsg,Toast.LENGTH_LONG).show();
     }
 }
